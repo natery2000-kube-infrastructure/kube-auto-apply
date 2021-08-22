ad29c9772cb63a5c40c559bd1cdb86bfdfa0cf89
Nathan <you@example.com>
2021-08-22 16:08:23 -0500

    Fixing github paths and kubectl command arguments

--- github.go
+++ github.go
@@ -15,44 +15,59 @@ func updateAndApplyFromGithub() {
 	var projectResponse ProjectResponse
 	projectUrl := "https://api.github.com/repos/natery2000/local-kube"
 
+	fmt.Println("Getting default branch name.")
 	getResponse(projectUrl, &projectResponse)
 	if projectResponse.DefaultBranch == "" {
 		fmt.Println("Failed to get project default branch")
-		return
+		//return
 	}
 
+	fmt.Println("Getting last commit to default branch date")
 	var branchResponse BranchResponse
 	branchUrl := projectUrl + "/branches/" + projectResponse.DefaultBranch
 
 	getResponse(branchUrl, &branchResponse)
 	if branchResponse.Commit.Commit.Committer.Date == "" {
 		fmt.Println("Failed to get last commit date")
-		return
+		//return
 	}
 
 	date, _ := time.Parse(time.RFC3339, branchResponse.Commit.Commit.Committer.Date)
 	fmt.Println(date)
 
 	if date.After(lastCommitDate) {
-		repo, _ := git.PlainOpen(projectUrl)
+		localRepositoryDirectory := "c:/Temp/local-kube"
+		repositoryUrl := "https://github.com/natery2000/local-kube"
 
-		repo, err := git.PlainClone("c:/Temp/local-kube", false, &git.CloneOptions{URL: projectUrl, Progress: os.Stdout})
+		fmt.Println("Cloning repository")
+		repo, err := git.PlainClone(localRepositoryDirectory, false, &git.CloneOptions{URL: repositoryUrl, Progress: os.Stdout})
 		if err != nil {
 			fmt.Println(err)
-			return
 		}
 
-		work, _ := repo.Worktree()
+		fmt.Println("Opening repository")
+		repo, err = git.PlainOpen(localRepositoryDirectory)
+		if err != nil {
+			fmt.Println(err)
+		}
+		fmt.Println("Getting Worktree")
+		work, err := repo.Worktree()
+		if err != nil {
+			fmt.Println(err)
+		}
 
+		fmt.Println("Pulling latest")
 		err = work.Pull(&git.PullOptions{RemoteName: "origin"})
+		if err != nil {
+			fmt.Println(err)
+		}
 
-		cmd := exec.Command("kubectl")
-		cmd.Args = []string{"apply -f . --recursive"}
+		fmt.Println("Applying new yaml files")
+		cmd := exec.Command("kubectl", "apply", "-f", ".", "--recursive")
 		cmd.Dir = "c:/Temp/local-kube"
-		out, ee := cmd.Output()
-
-		fmt.Println(string(out))
-		fmt.Println(ee)
+		cmd.Stdout = os.Stdout
+		cmd.Stderr = os.Stderr
+		cmd.Run()
 
 		lastCommitDate = date
 	} else {
