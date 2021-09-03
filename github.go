13ab9e40987820f9c146208e3b46025ee5d76d08
Nathan <you@example.com>
2021-09-03 00:02:15 -0500

    Simplifying git logic to not use rate-limited API

--- github.go
+++ github.go
@@ -12,65 +12,40 @@ import (
 var lastCommitDate time.Time = time.Date(0, 0, 0, 0, 0, 0, 0, time.UTC)
 
 func updateAndApplyFromGithub() {
-	var projectResponse ProjectResponse
-	projectUrl := "https://api.github.com/repos/natery2000/local-kube"
+	localRepositoryDirectory := "/repo"
+	repositoryUrl := "https://github.com/natery2000/local-kube"
 
-	fmt.Println("Getting default branch name.")
-	getResponse(projectUrl, &projectResponse)
-	if projectResponse.DefaultBranch == "" {
-		fmt.Println("Failed to get project default branch")
-		//return
+	fmt.Println("Cloning repository")
+	repo, err := git.PlainClone(localRepositoryDirectory, false, &git.CloneOptions{URL: repositoryUrl, Progress: os.Stdout})
+	if err != nil {
+		fmt.Println(err)
 	}
 
-	fmt.Println("Getting last commit to default branch date")
-	var branchResponse BranchResponse
-	branchUrl := projectUrl + "/branches/" + projectResponse.DefaultBranch
-
-	getResponse(branchUrl, &branchResponse)
-	if branchResponse.Commit.Commit.Committer.Date == "" {
-		fmt.Println("Failed to get last commit date")
-		//return
+	fmt.Println("Opening repository")
+	repo, err = git.PlainOpen(localRepositoryDirectory)
+	if err != nil {
+		fmt.Println(err)
+		return
 	}
 
-	date, _ := time.Parse(time.RFC3339, branchResponse.Commit.Commit.Committer.Date)
-	fmt.Println(date)
-
-	if date.After(lastCommitDate) {
-		localRepositoryDirectory := "/repo"
-		repositoryUrl := "https://github.com/natery2000/local-kube"
-
-		fmt.Println("Cloning repository")
-		repo, err := git.PlainClone(localRepositoryDirectory, false, &git.CloneOptions{URL: repositoryUrl, Progress: os.Stdout})
-		if err != nil {
-			fmt.Println(err)
-		}
-
-		fmt.Println("Opening repository")
-		repo, err = git.PlainOpen(localRepositoryDirectory)
-		if err != nil {
-			fmt.Println(err)
-		}
-		fmt.Println("Getting Worktree")
-		work, err := repo.Worktree()
-		if err != nil {
-			fmt.Println(err)
-		}
-
-		fmt.Println("Pulling latest")
-		err = work.Pull(&git.PullOptions{RemoteName: "origin"})
-		if err != nil {
-			fmt.Println(err)
-		}
-
-		fmt.Println("Applying new yaml files")
-		cmd := exec.Command("kubectl", "apply", "-f", ".", "--recursive")
-		cmd.Dir = localRepositoryDirectory
-		cmd.Stdout = os.Stdout
-		cmd.Stderr = os.Stderr
-		cmd.Run()
-
-		lastCommitDate = date
-	} else {
-		fmt.Println("Environment up to date")
+	fmt.Println("Getting Worktree")
+	work, err := repo.Worktree()
+	if err != nil {
+		fmt.Println(err)
+		return
 	}
+
+	fmt.Println("Pulling latest")
+	err = work.Pull(&git.PullOptions{RemoteName: "origin"})
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	fmt.Println("Applying new yaml files")
+	cmd := exec.Command("kubectl", "apply", "-f", ".", "--recursive")
+	cmd.Dir = localRepositoryDirectory
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	cmd.Run()
 }
