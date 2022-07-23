537f1bef4b565775e5cc012762ebd062063bc856
Nathan <you@example.com>
2022-07-23 13:13:33 -0500

    Adding debug prints

--- main.go
+++ main.go
@@ -1,15 +1,18 @@
 package main
 
 import (
+	"fmt"
 	"time"
 )
 
 func runAtInterval(what func(), delay time.Duration) {
 	for range time.Tick(delay) {
+		fmt.Println("tick")
 		what()
 	}
 }
 
 func main() {
+	fmt.Println("started")
 	runAtInterval(updateAndApplyFromGithub, 5*time.Minute)
 }
